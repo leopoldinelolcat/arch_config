c9951c5877afcc914712defbf4bd4db3ffbc636e
Tristan <thebert@bocasay.com>
2018-01-24 07:43:36 +0100

    ajout package

--- install/4.pacman.sh
+++ install/4.pacman.sh
@@ -1,6 +1,6 @@
 sudo pacman -Syu i3 dmenu feh git imagemagick leafpad openssh perl-digest-sha1 
 qutebrowser vim xfce4-terminal gnome-screenshot gnome-calculator ntfs-3g parted nmap uget unzip vlc 
-alsa-utils
+alsa-utils hdparm dosfstools
 # other
 # sudo pacman -Syu libreoffice-still apache composer mariadb php php-apache php-gd 
 php-intl php-mcrypt phpmyadmin mupdf cronie
