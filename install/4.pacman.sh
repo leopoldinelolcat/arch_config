81b8d30063e854685f711605314103bfe19e75ca
Tristan <thebert@bocasay.com>
2018-01-30 13:00:39 +0100

    add package, add profile conf

--- install/4.pacman.sh
+++ install/4.pacman.sh
@@ -1,5 +1,5 @@
 sudo pacman -Syu i3 dmenu feh git imagemagick leafpad openssh perl-digest-sha1 qutebrowser vim xfce4-terminal gnome-screenshot gnome-calculator ntfs-3g parted nmap uget unzip vlc 
-alsa-utils hdparm dosfstools xorg-server xorg-xinit
+alsa-utils hdparm dosfstools xorg-server xorg-xinit xorg-xrand
 # other
 # sudo pacman -Syu libreoffice-still apache composer mariadb php php-apache php-gd 
 php-intl php-mcrypt phpmyadmin mupdf cronie
