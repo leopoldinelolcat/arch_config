fc6ab58fb95fe26cf6ebcdae70179dfed0071804
thebert <thebert@bocasay.com>
2018-03-07 07:22:47 +0100

    remove php-mcrypt, deprecated

--- install/4.pacman.sh
+++ install/4.pacman.sh
@@ -2,6 +2,6 @@ sudo pacman -Syu i3 dmenu feh git imagemagick leafpad openssh perl-digest-sha1 q
 alsa-utils hdparm dosfstools xorg-server xorg-xinit xorg-xrand
 # other
 # sudo pacman -Syu libreoffice-still apache composer mariadb php php-apache php-gd 
-php-intl php-mcrypt phpmyadmin mupdf cronie
+php-intl phpmyadmin mupdf cronie
 # sudo pacman -Syu dhcpcd
 
